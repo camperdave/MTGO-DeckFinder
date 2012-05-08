a9eda09f120b84c013d486f6144e20d283b17ee2
David Lee <David Leecamperdave614@gmail.com>
2012-05-08 11:10:44 -0500

    Version 1.0 is ready! Icons still suck, but whatever. At least it seems to work on both types of deck listing :D

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
-function processCardClosure(cards) {
-	return (function processCard(index, element) {
+function processCardArchClosure(cards) {
+	return (function processCardArch(index, element) {
 		parent = $(this).parent().parent()
 		children = parent.children()
 
@@ -25,6 +25,36 @@ function processCardClosure(cards) {
 	})
 }
 
+function processCardBasicClosure(cards) {
+	return (function processCardBasic(index, element) {
+		parent = $(this).parent()
+
+		children = parent.contents()
+
+		quantity = $.trim($(children[0]).text())
+		name = $.trim($(children[1]).text())
+		price = $.trim($($(parent).parent().children()[2]).text())
+
+		num_quantity = parseInt(quantity)
+		if(cards[name] != null) {
+			var missing = num_quantity - cards[name]
+		}
+		else {
+			var missing = num_quantity
+		}
+		if (missing > 0) {
+			single_price = parseFloat(price.substring(1)) / num_quantity
+			cost_to_buy = single_price * missing
+			$(children[0]).before(quantity + " -> " + missing)
+			$(children[0]).remove()
+			$($(parent).parent().children()[2]).text(price + " -> " + missing + "x $" + single_price + " = $" + (Math.round(cost_to_buy * 100) / 100))
+		}
+		else {
+			$(parent).parent().css("text-decoration", "line-through");
+		}
+	})
+}
+
 chrome.extension.sendRequest({message: "GET_CSV"}, function(response) {
 	csv_cards = $.csv2Dictionary(response.csv)
 	have_cards = {}
@@ -35,11 +65,18 @@ chrome.extension.sendRequest({message: "GET_CSV"}, function(response) {
 		}
 		have_cards[card_line["Card Name"]] += parseInt(card_line["Online"])
 	}
-	processCardsGivenCSV = processCardClosure(have_cards)
-	$(".popular-card").each(processCardsGivenCSV)
-	$(".liked-card").each(processCardsGivenCSV)
-	$(".unused-card").each(processCardsGivenCSV)
-	
-	//$("td.nowrap > a").each(processCardsGivenCSV)
+
+	if( $(".popular-card").length > 0){
+		processCardsArchGivenCSV = processCardArchClosure(have_cards)
+
+		$(".popular-card").each(processCardsArchGivenCSV)
+		$(".liked-card").each(processCardsArchGivenCSV)
+		$(".unused-card").each(processCardsArchGivenCSV)
+	}
+	else {
+		processCardsBasicGivenCSV = processCardBasicClosure(have_cards)
+		$("td.nowrap > a").each(processCardsBasicGivenCSV)
+	}
+
 });
 
--- options.js
+++ options.js
@@ -5,12 +5,15 @@ function handleFileSelect(evt) {
 
 	var reader = new FileReader();
 
-	reader.onload = (function() {
-		var prefs = JSON.parse(localStorage.prefs)
-		prefs.csv = reader.result
-		localStorage.prefs = JSON.stringify(prefs)
-		document.write("File Processed!");
-	});
+	reader.onload = (function makeReaderFunction(name) {
+		return (function() {
+			var prefs = JSON.parse(localStorage.prefs)
+			prefs.csv = reader.result
+			prefs.csvName = name
+			localStorage.prefs = JSON.stringify(prefs)
+			document.write("File Processed!");
+		})
+	})(f.name)
 	
 	// Read in the image file as a data URL.
 	reader.readAsText(f);
@@ -20,5 +23,9 @@ function handleFileSelect(evt) {
 // Make sure the checkbox checked state gets properly initialized from the
 // saved preference.
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('csvFile').addEventListener('change', handleFileSelect, false);
+	document.getElementById('csvFile').addEventListener('change', handleFileSelect, false);
+	var prefs = JSON.parse(localStorage.prefs)
+	if(prefs.csvName != null) {
+		document.getElementById('currentFilename').innerHTML = "Current CSV File: " + prefs.csvName
+	}
 });
