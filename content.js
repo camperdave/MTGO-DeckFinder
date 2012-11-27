68b90c7bcffba9ad7f2ec027c29e7ddc358dae36
Dave Lee <camperdave614@gmail.com>
2012-11-27 08:28:33 -0600

    total cost on non-archtype decks

--- content.js
+++ content.js
@@ -30,27 +30,28 @@ function processCardArchClosure(cards) {
 
 function processCardBasicClosure(cards) {
 	return (function processCardBasic(index, element) {
-		parent = $(this).parent()
+		parent = $(this).parent();
 
-		children = parent.contents()
+		children = parent.contents();
 
-		quantity = $.trim($(children[0]).text())
-		name = $.trim($(children[1]).text())
-		price = $.trim($($(parent).parent().children()[2]).text())
+		quantity = $.trim($(children[0]).text());
+		name = $.trim($(children[1]).text());
+		price = $.trim($($(parent).parent().children()[2]).text());
 
 		num_quantity = parseInt(quantity)
 		if(cards[name] != null) {
-			var missing = num_quantity - cards[name]
+			var missing = num_quantity - cards[name];
 		}
 		else {
-			var missing = num_quantity
+			var missing = num_quantity;
 		}
 		if (missing > 0) {
-			single_price = parseFloat(price.substring(1)) / num_quantity
-			cost_to_buy = single_price * missing
-			$(children[0]).before(quantity + " -> " + missing)
-			$(children[0]).remove()
-			$($(parent).parent().children()[2]).text(price + " -> " + missing + "x $" + single_price + " = $" + (Math.round(cost_to_buy * 100) / 100))
+			single_price = parseFloat(price.substring(1)) / num_quantity;
+			cost_to_buy = single_price * missing;
+			total_cost_to_finish += cost_to_buy;
+			$(children[0]).before(quantity + " -> " + missing);
+			$(children[0]).remove();
+			$($(parent).parent().children()[2]).text(price + " -> " + missing + "x $" + single_price + " = $" + (Math.round(cost_to_buy * 100) / 100));
 		}
 		else {
 			$(parent).parent().css("text-decoration", "line-through");
@@ -71,17 +72,17 @@ chrome.extension.sendRequest({message: "GET_CSV"}, function(response) {
 	total_cost_to_finish = 0;
 	if( $("a.popular-card").length > 0){
 		processCardsArchGivenCSV = processCardArchClosure(have_cards)
-
 		$("a.popular-card").each(processCardsArchGivenCSV);
 		$("a.liked-card").each(processCardsArchGivenCSV);
 		$("a.unused-card").each(processCardsArchGivenCSV);
-		console.log(total_cost_to_finish);
 		var htmlTotalCostToBuy = '<tr><td class="bold">Cost to Finish:</td><td id="mtgo-deckfinder-total-cost-to-buy">$' + (Math.round(total_cost_to_finish * 100) / 100) + '</td></tr>'; 
 		$("td.bold:contains('Format')").parent().parent().append(htmlTotalCostToBuy);
 	}
 	else {
-		processCardsBasicGivenCSV = processCardBasicClosure(have_cards)
-		$("td.nowrap > a").each(processCardsBasicGivenCSV)
+		processCardsBasicGivenCSV = processCardBasicClosure(have_cards);
+		$("td.nowrap > a").each(processCardsBasicGivenCSV);
+		var htmlTotalCostToBuy = '<tr><td align="center" class="bold" colspan="2" id="mtgo-deckfinder-total-cost-to-buy">Cost to Finish: $' + (Math.round(total_cost_to_finish * 100) / 100) + '</td></tr>'; 
+		$("td.bold:contains('Main Deck')").parent().parent().append(htmlTotalCostToBuy);
 	}
 
 });
